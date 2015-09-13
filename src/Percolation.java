c674c117cf2bacef7be59f020848e643eeb39f0e
alexshch <alexander.v.shcherbakov@gmail.com>
2015-09-13 15:33:40 +0300

    have done

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,127 +1,180 @@
-/**
- * Created by SuN on 08.09.2015.
- */
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+/*----------------------------------------------------------------
+ *  Author:        Alex Shcherbakov
+ *  Written:       8/9/2015
+ *  Last updated:  12/9/2015
+ *
+ *  Compilation:   javac Percolation.java
+ *  Execution:     java Percolation
+ *
+ *  This programm calculates probability of percolation for percolation model size N
+ *
+ *  % java Percolation 100
+ *  treshhold  = 0.607700
+ *----------------------------------------------------------------*/
 
-import java.util.Random;
+//import edu.princeton.cs.algs4.StdOut;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+//import edu.princeton.cs.algs4.QuickFindUF;
 
 
 public class Percolation {
+
+    private WeightedQuickUnionUF openSitesGrid;     //union of open sites
+    private WeightedQuickUnionUF fullSitesGrid;     //union of open sites
+    private boolean[][] emptyOpenSites;             //map of open sites
+    private int sizeN;                              //grid size
+    private int top;                                //top element in WeightedQuickUnionUF
+    private int bottom;                             //bottom element in WeightedQuickUnionUF
+
     // create N-by-N grid, with all sites blocked
-    private WeightedQuickUnionUF openSitesGrid;
-    private boolean[][] emptyOpenSites;
-    private int SizeN;
     public Percolation(int N) {
-        if (N <= 0) throw new IndexOutOfBoundsException("too low N");
+        if (N <= 0) throw new IllegalArgumentException("too low N");
 
-        openSitesGrid = new WeightedQuickUnionUF(N*N);
+        openSitesGrid = new WeightedQuickUnionUF(N * N + 2); //openSitesGrid [N*N-2] - top, openSitesGrid [N*N-1] -bottom
+        fullSitesGrid = new WeightedQuickUnionUF(N * N + 1);
         emptyOpenSites = new boolean[N][N];
-        SizeN = N;
+        sizeN = N;
+        this.top = N * N;
+        this.bottom = N * N + 1;
 
-        }
+    }
 
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) {
+        if (i <= 0 || i > sizeN) throw new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > sizeN) throw new IndexOutOfBoundsException("column index j out of bounds");
 
-        int currentSite = transformDecardCoordToUnionCoord(i,j);
-        if (!isOpen(i,j)){
-            int[] neighbors = openNeighborSites(i,j);
-            for (int k=0; k<4; k++ ){
-                if (neighbors[k] != -1){
+        int currentSite = transformDecardCoordToUnionCoord(i, j); //the site which is opening
+        if (!isOpen(i, j)) {
+            int[] neighbors = openNeighbourSites(i, j);
+            if (i == 1) {
+                openSitesGrid.union(currentSite, this.top);
+                fullSitesGrid.union(currentSite, this.top);
+            }
+            if (i == sizeN)
+                openSitesGrid.union(currentSite, bottom);
+            for (int k = 0; k < 4; k++) {
+                if (neighbors[k] != -1) {
                     openSitesGrid.union(currentSite, neighbors[k]);
+                    fullSitesGrid.union(currentSite, neighbors[k]);
                 }
             }
 
-            emptyOpenSites[i-1][j-1] = true;
+
+            emptyOpenSites[i - 1][j - 1] = true;
 
         }
     }
+
     // transform Decard coordinates to element number in WeightedQuickUnionUF
-    private int transformDecardCoordToUnionCoord(int i, int j){
-        return (i-1)*SizeN+(j-1);
+    private int transformDecardCoordToUnionCoord(int i, int j) {
+        return (i - 1) * sizeN + (j - 1);
     }
 
-    //return a naighbours sites (-1 in the absent)
-    private int[] openNeighborSites(int i,int j){
-        //{left, right, up, down}
-        int[] neighbors = new int[4];
+    //return a massive of neighbour sites (-1 in the absent)
+    private int[] openNeighbourSites(int i, int j) {
 
+        int[] neighbours = new int[4]; //{left, right, up, down} neighbours of site
 
-        neighbors[0] = (j != 1) ? transformDecardCoordToUnionCoord(i, j - 1) : -1;
-        if (neighbors[0] != -1) {
+        //neighbours[0] = (j != 1) ? transformDecardCoordToUnionCoord(i, j - 1) : -1;
+        if (j != 1) {
+            neighbours[0] = transformDecardCoordToUnionCoord(i, j - 1);
+        } else {
+            neighbours[0] = -1;
+        }
+        if (neighbours[0] != -1) {
             if (!isOpen(i, j - 1)) {
-                neighbors[0] = -1;
+                neighbours[0] = -1;
             }
         }
-        neighbors[1] = (j != SizeN) ? transformDecardCoordToUnionCoord(i, j + 1) : -1;
-        if (neighbors[1] != -1) {
+        //neighbours[1] = (j != sizeN) ? transformDecardCoordToUnionCoord(i, j + 1) : -1;
+        if (j != sizeN) {
+            neighbours[1] = transformDecardCoordToUnionCoord(i, j + 1);
+        } else {
+            neighbours[1] = -1;
+        }
+        if (neighbours[1] != -1) {
             if (!isOpen(i, j + 1)) {
-                neighbors[1] = -1;
+                neighbours[1] = -1;
             }
         }
-        neighbors[2] = (i != 1) ? transformDecardCoordToUnionCoord(i - 1, j) : -1;
-        if (neighbors[2] != -1) {
+        //neighbours[2] = (i != 1) ? transformDecardCoordToUnionCoord(i - 1, j) : -1;
+        if (i != 1) {
+            neighbours[2] = transformDecardCoordToUnionCoord(i - 1, j);
+        } else {
+            neighbours[2] = -1;
+        }
+        if (neighbours[2] != -1) {
             if (!isOpen(i - 1, j)) {
-                neighbors[2] = -1;
+                neighbours[2] = -1;
             }
         }
-        neighbors[3] = (i != SizeN) ? transformDecardCoordToUnionCoord(i + 1, j) : -1;
-        if (neighbors[3] != -1) {
+        //neighbours[3] = (i != sizeN) ? transformDecardCoordToUnionCoord(i + 1, j) : -1;
+        if (i != sizeN) {
+            neighbours[3] = transformDecardCoordToUnionCoord(i + 1, j);
+        } else {
+            neighbours[3] = -1;
+        }
+        if (neighbours[3] != -1) {
             if (!isOpen(i + 1, j)) {
-                neighbors[3] = -1;
+                neighbours[3] = -1;
             }
         }
-        return neighbors;
+        return neighbours;
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        return emptyOpenSites[i-1][j-1];
+
+        if (i <= 0 || i > sizeN)
+            throw new java.lang.IndexOutOfBoundsException("i is out of range 1..N");
+        if (j <= 0 || j > sizeN)
+            throw new java.lang.IndexOutOfBoundsException("j is out of range 1..N");
+
+        return emptyOpenSites[i - 1][j - 1];
     }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        int currentSite = transformDecardCoordToUnionCoord(i,j);
-        for (int k = 1; k < SizeN+1; k++){
-            int topSite =  transformDecardCoordToUnionCoord(1,k);
-            if (isOpen(1,k) && openSitesGrid.connected(topSite,currentSite)){
-                return true;
-            }
-        }
-        return false;
-        //return  fullOpenSites[i-1][j-1];
+        if (i <= 0 || i > sizeN)
+            throw new java.lang.IndexOutOfBoundsException("i is out of range 1..N");
+        if (j <= 0 || j > sizeN)
+            throw new java.lang.IndexOutOfBoundsException("j is out of range 1..N");
+
+        int currentSite = transformDecardCoordToUnionCoord(i, j);
+
+        return fullSitesGrid.connected(this.top, currentSite);
     }
 
     // does the system percolate?
     public boolean percolates() {
-        for(int i=1; i<SizeN+1; i++ ){
-            if (isFull(SizeN,i))
-                return true;
-        }
+
+        if (openSitesGrid.connected(this.top, this.bottom))
+            return true;
+
         return false;
     }
 
     // test client (optional)
     public static void main(String[] args) {
 
-        int N = 100;
-        int trys = 0;
+        int N = Integer.parseInt(args[0]);
+        int tryCounter = 0;
         Percolation percolation = new Percolation(N);
         //StdRandom random = new StdRandom();
-        while(true){
-            int i = StdRandom.uniform(N)+1;
-            int j = StdRandom.uniform(N)+1;
+        while (true) {
+            int i = StdRandom.uniform(N) + 1;
+            int j = StdRandom.uniform(N) + 1;
             if (percolation.isOpen(i, j))
                 continue;
-            percolation.open(i,j);
-            trys++;
+            percolation.open(i, j);
+            tryCounter++;
             if (percolation.percolates())
                 break;
         }
 
-        System.out.println("final trys = "+String.valueOf(trys));
-        System.out.println("treshhold = "+String.valueOf(Double.valueOf(trys)/(N*N)));
+        //StdOut.printf("treshhold  = %f\n", Double.valueOf(tryCounter / (N * N)));
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,31 +1,88 @@
-/**
- * Created by SuN on 11.09.2015.
- */
-public class PercolationStats {
-    // perform T independent experiments on an N-by-N grid
-    public PercolationStats(int N, int T){
+/*----------------------------------------------------------------
+ *  Author:        Alex Shcherbakov
+ *  Written:       8/9/2015
+ *  Last updated:  12/9/2015
+ *
+ *  Compilation:   javac PercolationStats.java
+ *  Execution:     java PercolationStats
+ *
+ *  This programm calculates mean, standard derivation and 95% confidence
+ *  interval of percolation for percolation model size N
+ *
+ *  % java PercolationStats 200 100
+ *  treshhold  = 0.607700
+ *----------------------------------------------------------------*/
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class PercolationStats {
+
+    //private double meanThreshold;
+    //private double stddevThreshold;
+    private double[] results;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) throw new java.lang.IllegalArgumentException();
+
+        results = new double[T];
+        int tryCounter;
+        double threshold;
+
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = new Percolation(N);
+            tryCounter = 0;
+            while (true) {
+                int k = StdRandom.uniform(N) + 1;
+                int m = StdRandom.uniform(N) + 1;
+                if (percolation.isOpen(k, m))
+                    continue;
+                percolation.open(k, m);
+                tryCounter++;
+                if (percolation.percolates())
+                    break;
+            }
+            threshold = Double.valueOf(tryCounter) / (N * N);
+            results[i] = threshold;
+        }
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        Stopwatch stopwatch = new Stopwatch();
+        PercolationStats stats = new PercolationStats(N, T);
+        StdOut.printf("mean                    = %f\n", stats.mean());
+        StdOut.printf("stddev                  = %f\n", stats.stddev());
+        StdOut.printf("95%% confidence interval = %f, %f\n", stats.confidenceLo(), stats.confidenceHi());
+        double time = stopwatch.elapsedTime();
+        StdOut.printf("test time               = %f sec", time);
     }
 
     // sample mean of percolation threshold
-    public double mean(){
-        return 0;
+    public double mean() {
+        //meanThreshold =  StdStats.mean(results);
+        return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev(){
-        return 0;
+    public double stddev() {
+        //stddevThreshold = StdStats.stddev(results);
+        return StdStats.stddev(results);
     }
-    // low  endpoint of 95% confidence interval
-    public double confidenceLo(){
-        return 0;
-    }
-    // high endpoint of 95% confidence interval
-    public double confidenceHi(){
-        return 0;
-    }
-    // test client (described below)
-    public static void main(String[] args){
 
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return (mean() - 1.96 * stddev() / Math.sqrt(results.length));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return (mean() + 1.96 * stddev() / Math.sqrt(results.length));
     }
 }
